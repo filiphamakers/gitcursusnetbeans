77c371f9a93e80e42d6e78117667561c291a13b4
filiphamakers <filiphamakers@hotmail.com>
2017-07-10 15:05:39 +0200

    hallo

---
+++ Documents/NetBeansProjects/GitCursus/src/be/vdab/GitCursus.java
@@ -0,0 +1,22 @@
+package be.vdab;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Filip
+ */
+public class GitCursus {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hallo");
+    }
+    
+}
